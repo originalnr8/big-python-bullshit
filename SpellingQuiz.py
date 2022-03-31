8f1551dbbaba17fe30e3300bc2341dab463f906d
originalnr8 <50736313+originalnr8@users.noreply.github.com>
2022-03-31 20:51:29 +0100

    Initial

---
+++ SpellingQuiz.py
@@ -0,0 +1,55 @@
+import random
+# Spelling Quiz
+quiz = [
+    ["What structure do people occupy? ", "building"], 
+    ["What animal makes the noise MOO? ", "cow"]
+    ]
+
+def askQuestion_string(question):
+    return str(input(question))
+
+def askQuestion_integer(question):
+    try:
+        answer = int(askQuestion_string(question))
+    except:
+        print("I didn't understand the number you entered.")
+        answer = False
+    return answer
+
+def compareSpelling(attempt, actual):
+    listAttempt = list(attempt)
+    listActual = list(actual)
+    for i in range(len(actual)):
+        if listAttempt[i] == listActual[i]:
+            #output.join(actual[i:1])
+            j=0
+        else:
+            listActual[i] = "_"
+    output = "".join(listActual)
+    return output
+
+def checkMisspelling(word):
+    for x in range(len(word)):
+        if word[x:1] == '_':
+            return False
+    return True
+
+def SpellingQuiz():
+    leave = False
+    while leave == False:
+        index = random.randint(0,len(quiz)-1)
+        answer = quiz[index][1].lower()
+        next = False
+        while next == False:
+            guess = askQuestion_string(quiz[index][0]).lower()
+            if guess == "leave":
+                leave = True
+            incorrect = compareSpelling(guess, answer)
+            if checkMisspelling(incorrect) == False:
+                print("The word you entered does not match my answer")
+                print("This is what I understood {0}".format(incorrect))
+            else:
+                print("Wonderful spelling. Well done")
+                next = True
+
+SpellingQuiz()
